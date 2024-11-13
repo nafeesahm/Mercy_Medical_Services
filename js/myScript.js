2b2f9994c4b733cbef2fa01cc2aa9f40bef5ade6
Nafees Ahmad <nafees.ahmad@dynatacorp.com>
2024-11-14 04:30:02 +0530

    Mercy Medical Services

---
+++ js/myScript.js
@@ -0,0 +1,15 @@
+    // Text to display
+    const text = "Mercy Medical Services";
+    let index = 0;
+
+    function showText() {
+      // If there's more text to display
+      if (index < text.length) {
+        document.getElementById('assistant-text').textContent += text[index];
+        index++;
+        setTimeout(showText, 50); // Adjusted speed: 50 milliseconds between characters
+      }
+    }
+
+    // Start displaying the text
+    showText();
